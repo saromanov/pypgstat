db384f9d4b970a956eafb7210c70cb2d2a2bb390
Sergey <xxsmotur@gmail.com>
2020-06-06 11:55:50 +0500

    Add method for get anomalies

--- pypgstat/pg_stat_database.py
+++ pypgstat/pg_stat_database.py
@@ -12,7 +12,14 @@ class PgStatDatabase(Table):
         Table.__init__(self, connection)
     
     def result(self):
-        self._get_basic_metrics()
+        self._get_anomaly()
+    
+    def _get_anomaly(self):
+        result = self._connection.execute(f'select datname, (xact_commit*100)/(xact_commit+xact_rollback) as c_ratio, deadlocks, conflicts, temp_files, pg_size_pretty(temp_bytes) as temp_size from {self.PG_STAT_DATABASE}')
+        for r in result:
+            print(result)
     
     def _get_basic_metrics(self):
-        self._connection.execute(f'SELECT * FROM {self.PG_STAT_DATABASE}')
+        result = self._connection.execute(f'SELECT * FROM {self.PG_STAT_DATABASE}')
+        for r in result:
+            print(r)

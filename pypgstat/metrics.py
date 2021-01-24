792eb1810b76787feb117c3de3957d7dfd135c11
Sergey <xxsmotur@gmail.com>
2021-01-24 14:28:09 +0500

    Draft for insert data

--- pypgstat/metrics.py
+++ pypgstat/metrics.py
@@ -2,6 +2,7 @@ import argparse
 import sqlalchemy as db
 from pg_stat_database import PgStatDatabase
 from pg_stat_activity import PgStatActivity
+from writer import Writer
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--host', type=str, default='localhost')
@@ -14,7 +15,7 @@ connect = engine.connect()
 
 engine_metrics = db.create_engine(f'postgresql://{args.user}:{args.password}@{args.host}:5432/{args.db}')
 connect_metrics = engine.connect()
-
-pgsd = [PgStatDatabase(connect, table_name='tracer', metrics_db=connect_metrics), PgStatActivity(connect, metrics_db=connect_metrics)]
+w = Writer(connect_metrics)
+pgsd = [PgStatDatabase(connect, table_name='tracer', metrics_db=w), PgStatActivity(connect, metrics_db=w)]
 for p in pgsd:
     p.result()
